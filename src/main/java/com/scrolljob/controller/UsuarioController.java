35683462c7241a12331baa83cedca90c50841804
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2026-06-20 19:03:19 -0300

    Atualizando api para confirmar login

--- src/main/java/com/scrolljob/controller/UsuarioController.java
+++ src/main/java/com/scrolljob/controller/UsuarioController.java
@@ -1,15 +1,12 @@
 package com.scrolljob.controller;
 
-import com.scrolljob.dto.UsuarioDTO;
-import com.scrolljob.entity.Usuario;
+import com.scrolljob.dto.LoginDTO;
 import com.scrolljob.service.UsuarioService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin(origins = "*")// permite conectar com frontend
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 
     private final UsuarioService service;
@@ -18,28 +15,9 @@ public class UsuarioController {
         this.service = service;
     }
 
-    @PostMapping
-    public Usuario criar(@RequestBody UsuarioDTO dto) {
-        return service.criar(dto);
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDTO dto) {
+        return service.login(dto);
     }
-
-    @GetMapping
-    public List<Usuario> listar() {
-        return service.listar();
-    }
-
-    @GetMapping("/{id}")
-    public Usuario buscar(@PathVariable Long id) {
-        return service.buscarPorId(id);
-    }
-
-    @PutMapping("/{id}")
-    public Usuario atualizar(@PathVariable Long id, @RequestBody UsuarioDTO dto) {
-        return service.atualizar(id, dto);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id) {
-        service.deletar(id);
-    }
+    
 }
